df7bd4fb390effa675cdbd11af2f5a14e225a1d3
pratiksha033 <pratikshaparihar679@gmail.com>
2026-01-08 18:18:47 +0530

    fixed dark mode toggle bar

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </NextThemesProvider>
   );
